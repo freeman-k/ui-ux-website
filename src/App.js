12f5001108f74ad1fa54ac4ff020087f3247f98f
Kevin Freeman <kevin.freemansr@gmail.com>
2022-06-06 20:27:13 -0400

    feat: add containers and components
    The initiation of containers and components establishes a page layout that mimics the specs of the design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
